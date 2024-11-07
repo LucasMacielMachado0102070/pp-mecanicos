11792c3a673c44b8c975dfb305731515c74b598b
Lucas <lucasmachado010207@gmail.com>
2024-11-07 17:51:13 -0300

    Outro teste deploy

--- front/script.js
+++ front/script.js
@@ -7,7 +7,7 @@ button.onclick = async function (e) {
     let senha = document.getElementById("senha").value;
     let data = {nome,senha}
 
-    const response = await fetch('http://https://pp-mecanicos.vercel.app/api/login', {
+    const response = await fetch('http://pp-mecanicos.vercel.app/api/login', {
         method: "POST",
         headers: {"Content-type": "application/json;charset=UTF-8"},
         body: JSON.stringify(data)
